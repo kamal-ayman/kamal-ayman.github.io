47e622c0ac733188e1f643f3ec8fa2aa80bfeeb0
kamal-ayman <kamal.ayman522@gmail.com>
2026-01-15 17:50:08 +0200

    refactor: enhance hero section background with new animations, replace Font Awesome Flutter icons with SVG, and remove the projects section.

--- scripts/packages.js
+++ scripts/packages.js
@@ -72,7 +72,9 @@ class PackagesRenderer {
                         <div class="package-badges">
                             <span class="package-version">v${pkg.version}</span>
                             <span class="package-platform">
-                                <i class="fab fa-flutter"></i>
+                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor" style="width:14px;height:14px;">
+                                    <path d="M14.314 0L2.3 12 6 15.7 21.684.013h-7.357L14.314 0zm.014 11.072L7.857 17.53l6.47 6.47H21.7l-6.46-6.468 6.46-6.46h-7.37l-.002-.001z"/>
+                                </svg>
                                 ${pkg.platform}
                             </span>
                         </div>
